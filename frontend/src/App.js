969667959a4a851991eb20ea0dedca275d8d757b
mehiarss1991 <mehiarss1991@gmail.com>
2020-06-04 12:27:09 +0300

    web

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,72 +1,36 @@
 import React from "react";
-<<<<<<< HEAD
-import "./App.css";
-import Home from "./components/Home/Home";
-import Header from "./components/Header/Header";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fab } from "@fortawesome/free-brands-svg-icons";
-
-library.add(fab);
-
-function App() {
-  return (
-    <div>
-      <Header />
-      <Home />
-=======
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import {Regist, Login, Tires,Prepurches, DiagnoseFix} from "./login"
-
-
-//class App extends React.Component{
-
+import ReactDom from "react-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Regist, Login, Tires, Prepurches, DiagnoseFix } from "./login";
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <Switch>
-
-      <Route exact path="/">
-        <a href="/login">
-          Login 
-        </a>
-        <a href="/SignUp">
-          SignUp 
-        </a>
-        <a href="/tires">
-          Tires 
-        </a>
-        <a href="/Prepurches">
-          pre-purches 
-        </a>
-        <a href="/Diagnose">
-        Diagnose 
-        </a>
+        <Switch>
+          <Route exact path="/">
+            <a href="/login">Login</a>
+            <a href="/SignUp">SignUp</a>
+            <a href="/tires">Tires</a>
+            <a href="/Prepurches">pre-purches</a>
+            <a href="/Diagnose">Diagnose</a>
           </Route>
 
           <Route exact path="/Diagnose">
-            <DiagnoseFix/>
+            <DiagnoseFix />
           </Route>
 
           <Route exact path="/login">
-            <Login/>
+            <Login />
           </Route>
           <Route exact path="/Tires">
-            <Tires/>
+            <Tires />
           </Route>
           <Route exact path="/Prepurches">
-            <Prepurches/>
+            <Prepurches />
           </Route>
-      </Switch>
-
+        </Switch>
       </Router>
->>>>>>> amirbranch
     </div>
   );
 }
