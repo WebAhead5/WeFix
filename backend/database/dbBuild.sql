fcc828981865bf32352b6d73c2008fa9553fda0f
amirfahoum <amirfahoum@gmail.com>
2020-06-04 11:48:32 +0300

    merge

--- backend/database/dbBuild.sql
+++ backend/database/dbBuild.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-DROP TABLE IF EXISTS users, cars,tires,parts,reservations CASCADE;
+DROP TABLE IF EXISTS users, cars,tires,parts,cart;
 
 CREATE TABLE users (
 user_id SERIAL PRIMARY KEY,
@@ -16,8 +16,8 @@ car_id SERIAL PRIMARY KEY,
 manufacturer VARCHAR(100) NOT NULL,
 model VARCHAR(100) NOT NULL,
 make_year  INTEGER NOT NULL,
-last_service DATE   NOT NULL
-
+last_service DATE   NOT NULL,
+full_name VARCHAR(100) UNIQUE NOT NULL
 );
 
 CREATE TABLE tires(
@@ -39,25 +39,26 @@ working_hours REAL NOT NULL,
 amount INTEGER NOT NULL
 );
 
-CREATE TABLE reservations(
+CREATE TABLE cart(
 reservation_id Serial PRIMARY KEY,
-full_name VARCHAR(100) NOT NULL,
-manufacturer VARCHAR(100) NOT NULL,
-res_date DATE NOT NULL,
-description VARCHAR(100),
-address VARCHAR(100)
+item VARCHAR(100) NOT NULL,
+quantity INTEGER NOT NULL,
+price INTEGER NOT NULL,
+part_id INTEGER NOT NULL,
+user_id INTEGER NOT NULL
 );
+
 
 
 insert into users (full_name, email, password,city,zip) values ('amirfahoum', 'amirfahoum@gmail.com', '123456','NAZARETH',1110);
 insert into users (full_name, email, password,city,zip) values ('moris', 'moris@gmail.com', '123456','haifa',120);
 insert into users (full_name, email, password,city,zip) values ('mehiar', 'mehiar@gmail.com', '123456','telaviv',124);
 
-
-insert into cars ( manufacturer, model, make_year,last_service) values ('Bmw', 'm3', '2020','2020-06-16');
-insert into cars ( manufacturer, model, make_year,last_service) values ('seat', 'ibiza', '2020','2020-08-16');
-insert into cars ( manufacturer, model, make_year,last_service) values ('Mazda', 'mx5', '2020','2020-09-16');
-insert into cars ( manufacturer, model, make_year,last_service) values ('Volvo', 'c1', '2020','2020-06-18');
+/*
+insert into cars ( manufacturer, model, make_year,last_service,full_name) values ('Bmw', 'm3', '2020','2020-06-16','amirfahoum');
+insert into cars ( manufacturer, model, make_year,last_service,full_name) values ('seat', 'ibiza', '2020','2020-08-16','moris');
+insert into cars ( manufacturer, model, make_year,last_service,full_name) values ('Mazda', 'mx5', '2020','2020-09-16','mehiar');
+insert into cars ( manufacturer, model, make_year,last_service,full_name) values ('Volvo', 'c1', '2020','2020-06-18','Jake');
 
 
 
@@ -70,9 +71,10 @@ insert into tires ( manufacturer, model_name, dimentions,description,working_hou
 insert into parts (part_name,part_serialnumber,description,working_hours,amount) values ('Breaks', 1120, 'Breaks comes as a set X2',2,1);
 insert into parts (part_name,part_serialnumber,description,working_hours,amount) values ('HandBreak', 1125, 'Breaks X1',1.5,2);
 
+*/
+insert into cart (item, quantity, price,part_id,user_id) values ('Breaks',2,200,110,1);
+insert into cart (item, quantity, price,part_id,user_id) values ('HANDBREAKS',1,300,112,1);
 
-insert into reservations (full_name, manufacturer, res_date, description,address) values ('Amirfahoum','tires service','2020-10-16','X2 tires Maxis R1 with dimention 120/115','nazareth 110');
 
 
-
-COMMIT;
+COMMIT
--- backend/database/queries.js
+++ backend/database/queries.js
@@ -14,4 +14,11 @@ function postNewUser(full_name, email, password) {
     database.query(`insert into users (full_name, email, password) values ('${full_name}', '${email}', ${password}');`)
 }
 
+function addCar(manufacturer,model,make_year,last_service,full_name)
+{
+    database.query(`insert into cars (manufacturer, model, make_year,last_service,full_name) values ('${manufacturer}', '${model}', '${make_year}', '${last_service}',${full_name}');`)
+
+}
+
+
 
