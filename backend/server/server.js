7b47c734d0d6f7ebf43d0895d9160ecf81154025
amirfahoum <amirfahoum@gmail.com>
2020-06-08 16:54:32 +0300

    amir Branch updates

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,21 +1,11 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-<<<<<<< HEAD
 // const Pool = require("../database/dbConnection");
 
 //middleware
-=======
-const router=require("../controllers/roots")
-
-
-
-require('dotenv').config();
-app.use(express.json())
->>>>>>> 4f0d03db8dcb9c92d07c797d8d872e1c1d35bc2a
 app.use(cors());
 
-<<<<<<< HEAD
 //ROUTES//
 
 app.post("/cars", async (req, res) => {
@@ -33,12 +23,6 @@ app.post("/cars", async (req, res) => {
 app.get("/", (req, res) => {
   res.send("Hello five world");
 });
-=======
-app.use(router);
-
-
-
->>>>>>> 4f0d03db8dcb9c92d07c797d8d872e1c1d35bc2a
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
